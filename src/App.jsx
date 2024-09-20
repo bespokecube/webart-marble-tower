fc15307b80705dbcf5e057b34deb2f8cb737caee
Oleg Rybin <you@example.com>
2024-09-19 21:25:28 -0400

    Integrating a performance monitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
-import { ToneMappingMode, BlendFunction } from "postprocessing";
-import { Noise, EffectComposer, ToneMapping, TiltShift2, Bloom } from "@react-three/postprocessing";
+import { ToneMappingMode } from "postprocessing";
+import { Perf } from "r3f-perf";
+import { Noise, EffectComposer, ToneMapping, Bloom } from "@react-three/postprocessing";
 import { Tower } from "./Tower";
 
 const Scene = () => {
@@ -15,28 +16,35 @@ const Scene = () => {
   return (
     <>
       <Tower scale={scaleFactor} position={[scaleFactor * 1.2, scaleFactor * 2.6, 0]} />
-      <pointLight position={[-2, 5, 6]} intensity={1516} decay={1.8} distance={10} castShadow shadow-bias={0} />
+      <pointLight position={[-2, 5, 6]} intensity={1516} decay={1.8} distance={10} />
     </>
   );
 };
 
 const App = () => {
+  const [isPerfMonitorEnabled, setIsPerfMonitorEnabled] = useState(false);
+
   return (
-    <Canvas dpr={[1, 2]} orthographic camera={{ zoom: 220, position: [-2, 2, 9], rotation: [0, 0, 0] }}>
-      <OrbitControls
-        enableZoom={false}
-        enablePan={false}
-        maxPolarAngle={Math.PI / 2.3}
-        minPolarAngle={Math.PI / 2.5}
-        maxAzimuthAngle={Math.PI / 2000}
-        minAzimuthAngle={-Math.PI / 6}
-      />
-      <Scene />
-      <EffectComposer>
-        <ToneMapping mode={ToneMappingMode.NEUTRAL} />
-        <Bloom mipmapBlur intensity={0.3} luminanceThreshold={0.3} levels={3} luminanceSmoothing={0.4} />
-      </EffectComposer>
-    </Canvas>
+    <>
+      <button className='perf-monitor-toggle-btn' onClick={() => setIsPerfMonitorEnabled(prevState => !prevState)}></button>
+
+      <Canvas dpr={[1, 2]} orthographic camera={{ zoom: 220, position: [-2, 2, 9], rotation: [0, 0, 0] }}>
+        {isPerfMonitorEnabled && <Perf position='top-left' />}
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2.3}
+          minPolarAngle={Math.PI / 2.5}
+          maxAzimuthAngle={Math.PI / 2000}
+          minAzimuthAngle={-Math.PI / 6}
+        />
+        <Scene />
+        <EffectComposer>
+          <ToneMapping mode={ToneMappingMode.NEUTRAL} />
+          <Bloom mipmapBlur intensity={0.3} luminanceThreshold={0.3} levels={3} luminanceSmoothing={0.4} />
+        </EffectComposer>
+      </Canvas>
+    </>
   );
 };
 
